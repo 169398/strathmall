e0022dd40becea56662af600a8b360650df4ca2f
idris <kulubiidris@gmail.com>
2024-08-03 17:43:20 +0300

    chore: Update OrdersPage component to include buyer name in table

--- app/user/orders/page.tsx
+++ app/user/orders/page.tsx
@@ -1,4 +1,7 @@
-import Pagination from '@/components/shared/pagination'
+import { auth } from "@/auth";
+import DeleteDialog from "@/components/shared/delete-dialog";
+import Pagination from "@/components/shared/pagination";
+import { Button } from "@/components/ui/button";
 import {
   Table,
   TableBody,
@@ -6,36 +9,44 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from '@/components/ui/table'
-import { getMySellerOrders } from '@/lib/actions/sellerorder.actions'
-import { APP_NAME } from '@/lib/constants'
-import { formatCurrency, formatDateTime } from '@/lib/utils'
-import { Metadata } from 'next'
-import Link from 'next/link'
+} from "@/components/ui/table";
+import {
+  getAllSellerOrders,
+  handleDeleteOrder,
+} from "@/lib/actions/sellerorder.actions";
+import { APP_NAME } from "@/lib/constants";
+import { formatCurrency, formatDateTime, formatId } from "@/lib/utils";
+import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: `My Orders - ${APP_NAME}`,
-}
+};
+
 export default async function OrdersPage({
-  searchParams,
+  searchParams: { page = "1" },
 }: {
-  searchParams: { page: string }
+  searchParams: { page: string };
 }) {
-  const page = Number(searchParams.page) || 1
-  const orders = await getMySellerOrders({
-    page,
-    sellerId:'',
-    limit: 6,
-  })
+  const session = await auth();
+  if (session?.user.role !== "user")
+    throw new Error("user permission required");
+  const sellerId = session.user.id || "";
+  const orders = await getAllSellerOrders({
+    page: Number(page),
+    sellerId,
+  });
+
   return (
     <div className="space-y-2">
-      <h2 className="h2-bold">Orders</h2>
+      <h1 className="h2-bold">Orders</h1>
       <div className="overflow-x-auto">
         <Table>
           <TableHeader>
             <TableRow>
               <TableHead>ID</TableHead>
               <TableHead>DATE</TableHead>
+              <TableHead>BUYER</TableHead>
               <TableHead>TOTAL</TableHead>
               <TableHead>PAID</TableHead>
               <TableHead>DELIVERED</TableHead>
@@ -45,25 +56,29 @@ export default async function OrdersPage({
           <TableBody>
             {orders.data.map((order) => (
               <TableRow key={order.id}>
-                <TableCell>{order.id.substring(20, 24)}</TableCell>
+                <TableCell>{formatId(order.id)}</TableCell>
                 <TableCell>
                   {formatDateTime(order.createdAt).dateTime}
+                </TableCell>
+                <TableCell>
+                  {order.user ? order.user.name : "Deleted user"}
                 </TableCell>
                 <TableCell>{formatCurrency(order.totalPrice)}</TableCell>
                 <TableCell>
                   {order.isPaid && order.paidAt
                     ? formatDateTime(order.paidAt).dateTime
-                    : 'not paid'}
+                    : "not paid"}
                 </TableCell>
                 <TableCell>
                   {order.isDelivered && order.deliveredAt
                     ? formatDateTime(order.deliveredAt).dateTime
-                    : 'not delivered'}
+                    : "not delivered"}
                 </TableCell>
-                <TableCell>
-                  <Link href={`/order/${order.id}`}>
-                    <span className="px-2">Details</span>
-                  </Link>
+                <TableCell className="flex gap-1">
+                  <Button asChild variant="outline" size="sm">
+                    <Link href={`/order/${order.id}`}>Details</Link>
+                  </Button>
+                  <DeleteDialog id={order.id} action={handleDeleteOrder} />
                 </TableCell>
               </TableRow>
             ))}
@@ -74,5 +89,5 @@ export default async function OrdersPage({
         )}
       </div>
     </div>
-  )
+  );
 }
