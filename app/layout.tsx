5b53789d13c501d2e209ac1702533a1d6b3509d8
idris <kulubiidris@gmail.com>
2024-08-12 00:17:39 +0300

    feat:Update  start selling page to be accessible to authenticated users using session from the session storage

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import React from 'react'
 import Navbar from '@/components/shared/Navbar'
 import { constructMetadata } from '@/lib/paypal'
 import { Analytics } from "@vercel/analytics/react";
+import { SessionProvider } from 'next-auth/react'
 const fontSans = FontSans({
   subsets: ['latin'],
   weight: ['400', '600'],
@@ -19,22 +20,21 @@ export const metadata: Metadata = constructMetadata()
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode,
 }>) {
   return (
     <html lang="en">
       <body
         className={cn(
-          'min-h-screen bg-background font-sans antialiased',
+          "min-h-screen bg-background font-sans antialiased",
           fontSans.variable
         )}
       >
-        
-          <Navbar/>
-          {children}
-          <Analytics/>
-          <Toaster />
+        <Navbar />
+        <SessionProvider >{children}</SessionProvider>
+        <Analytics />
+        <Toaster />
       </body>
     </html>
-  )
+  );
 }
