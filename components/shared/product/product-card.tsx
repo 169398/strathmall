af016580188df3a01d849cf668a7d75232acd2d4
idris <kulubiidris@gmail.com>
2024-08-12 19:04:16 +0300

    chore: Update product card layout for better responsiveness

--- components/shared/product/product-card.tsx
+++ components/shared/product/product-card.tsx
@@ -1,11 +1,10 @@
-import Image from 'next/image'
-import Link from 'next/link'
-
-import { Card, CardContent, CardHeader } from '@/components/ui/card'
-import ProductPrice from './product-price'
-import Rating from './rating'
-import { Button } from '@/components/ui/button'
-import { product } from '@/types/sellerindex'
+import Image from "next/image";
+import Link from "next/link";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import ProductPrice from "./product-price";
+import Rating from "./rating";
+import { Button } from "@/components/ui/button";
+import { product } from "@/types/sellerindex";
 
 const ProductCard = ({ product }: { product: product }) => {
   return (
@@ -21,21 +20,26 @@ const ProductCard = ({ product }: { product: product }) => {
           />
         </Link>
       </CardHeader>
-      <CardContent className="p-4 grid gap-4">
+      <CardContent className="p-2 sm:p-4 grid gap-2 sm:gap-4">
         <div>
-          <p className="text-xs">{product.brand}</p>
+          <p className="text-xs sm:text-sm">{product.brand}</p>
         </div>
         <div>
           <Link href={`/product/${product.slug}`}>
-            <h2 className="text-sm font-medium">{product.name}</h2>
+            <h2 className="text-sm sm:text-base font-medium">{product.name}</h2>
           </Link>
         </div>
-        <div className="flex-between gap-4">
-          <Rating value={Number(product.rating)} />
+        <div className="flex flex-col gap-1">
+          <Rating
+            value={Number(product.rating)}
+          />
           {product.stock > 0 ? (
-            <ProductPrice value={Number(product.price)} />
+            <ProductPrice
+              value={Number(product.price)}
+              className="text-xs sm:text-sm"
+            />
           ) : (
-            <p className="text-destructive">Out of Stock</p>
+            <p className="text-xs sm:text-sm text-destructive">Out of Stock</p>
           )}
         </div>
         <div>
@@ -43,14 +47,18 @@ const ProductCard = ({ product }: { product: product }) => {
             href={`/quickview/product/${[product.slug]}`}
             className="w-full"
           >
-            <Button variant="outline" size="sm" className="flex gap-2 w-full">
+            <Button
+              variant="outline"
+              size="sm"
+              className="flex gap-2 w-full text-xs sm:text-sm"
+            >
               <span>Quick View</span>
             </Button>
           </Link>
         </div>
       </CardContent>
     </Card>
-  )
-}
+  );
+};
 
-export default ProductCard
+export default ProductCard;
