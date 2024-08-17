8254e4f7ad618424e04fb87463966b89b8a0b414
idris <kulubiidris@gmail.com>
2024-08-17 10:10:35 +0300

    feat: Update carousel scroll behavior in home product list

--- components/shared/product/home-productlist.tsx
+++ components/shared/product/home-productlist.tsx
@@ -20,18 +20,19 @@ const ProductList: React.FC<ProductListProps> = ({ title, data }) => {
       <h2 className="h2-bold mb-4">{title}</h2>
       {data.length > 0 ? (
         <div className="relative">
-          <Carousel className="w-full">
-            <CarouselContent className="flex space-x-4">
+          <Carousel className="w-full overflow-x-auto scroll-snap-x mandatory">
+            <CarouselContent className="flex space-x-4 scroll-snap-align-start">
               {data.map((product: product) => (
-                <CarouselItem key={product.slug} className="flex-none w-1/2 lg:w-1/4">
+                <CarouselItem key={product.slug} className="flex-none w-1/2 lg:w-1/4 scroll-snap-align-start">
                   <div className="p-1">
                     <ProductCard product={product} />
                   </div>
                 </CarouselItem>
               ))}
             </CarouselContent>
-            <CarouselPrevious className="absolute top-1/2 left-0 transform -translate-y-1/2 z-10 bg-gray-800 text-white p-2 rounded-full" />
-            <CarouselNext className="absolute top-1/2 right-0 transform -translate-y-1/2 z-10 bg-gray-800 text-white p-2 rounded-full" />
+            {/* Hide buttons on small screens */}
+            <CarouselPrevious className="hidden md:flex absolute top-1/2 left-0 transform -translate-y-1/2 z-10 bg-gray-800 text-white p-2 rounded-full" />
+            <CarouselNext className="hidden md:flex absolute top-1/2 right-0 transform -translate-y-1/2 z-10 bg-gray-800 text-white p-2 rounded-full" />
           </Carousel>
         </div>
       ) : (
