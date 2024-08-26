6e6ae32b51f1b0d8cdaee6c6737a1b3ed5e4cfea
idris <kulubiidris@gmail.com>
2024-08-26 23:31:05 +0300

    feat:Add product categories
    This allow vendors to only add categories from the specified ones rather than random categories

---
+++ lib/categories.ts
@@ -0,0 +1,26 @@
+export const categories = [
+    "Electronics",
+    "Shoes",
+    "Phones",
+    "Computer accessories",
+    "Gaming",
+    "Earpods",
+    "Men's Clothing",
+    "Women's Clothing",
+    "Books",
+    "Luggages & Bags",
+    "Home & Kitchen",
+    "Toys & Entertainment",
+    "Beauty & Personal Care",
+    "Bakery",
+    "Jewellery & Watches",
+    "Furniture",
+    "Sports and Entertainment",
+    "Hair extensions & Wigs"
+
+
+   
+
+    
+  ];
+  
