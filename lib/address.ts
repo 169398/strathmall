606528592b86d5a910dbfd74b168baf89ee3b080
idris <kulubiidris@gmail.com>
2024-09-01 13:53:44 +0300

    chore:Update shipping address display in PlaceOrderPage

--- lib/address.ts
+++ lib/address.ts
@@ -1,7 +1,7 @@
 // address.ts
 export const towns = [
-  { name: "Town A", shippingPrice: 50 },
-  { name: "Town B", shippingPrice: 70 },
-  { name: "Town C", shippingPrice: 100 },
+  { name: "Madaraka,Nairobi West,Ngara,Parklands,Kenyatta", shippingPrice: 130 },
+  { name: "WestLands,Kitengela,South C,South B,Langata,Kilimani,Ngummo,Eastleigh", shippingPrice: 200 },
+  { name: "Jamuhuri,Dagoreti corner,Kangemi,Kawangware,Livington,Muthaiga", shippingPrice: 290 },
   // Add more towns as needed
 ];
