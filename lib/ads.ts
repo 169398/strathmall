9a6c326cbfde478b0d5a2a4f51998d40782b7437
idris <kulubiidris@gmail.com>
2024-09-12 23:10:53 +0300

    Refactor ads.ts to update image paths

--- lib/ads.ts
+++ lib/ads.ts
@@ -1,4 +1,7 @@
 export const ads = [
-  "/assets/images/Amac-logo.jpeg",
+  "/assets/images/Black Simple Watch Promotion Advert Web Banner.png",
+  "/assets/images/White and Red Modern Electronic product Listing Amazon Product Image.png",
+
   "/assets/images/amacrice.png",
+
 ];
